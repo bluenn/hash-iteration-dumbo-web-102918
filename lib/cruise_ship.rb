2d7f4ef4c5de310fb5743993ac599d7a0451e3d7
a lan <bluenowte97@gmail.com>
2018-11-08 18:05:44 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,11 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
-
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
+#pick winner in suite a with name starts with A
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
